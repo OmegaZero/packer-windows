a4f0a2c546727c30c66ad683955862d701808459
Buck Jeffcott <buck.jeffcott@gmail.com>
2023-11-12 17:51:38 -0500

    Reduced need for elevated priveldges on chocolatey and 7zip

--- scripts/install-spice-guest-tools.ps1
+++ scripts/install-spice-guest-tools.ps1
@@ -9,7 +9,7 @@ if ("$env:PACKER_BUILDER_TYPE" -eq "qemu") {
         (New-Object System.Net.WebClient).DownloadFile('https://www.spice-space.org/download/windows/vdagent/vdagent-win-0.10.0/spice-vdagent-x64-0.10.0.msi', 'spice-vdagent.msi')
         Start-Process -FilePath msiexec -ArgumentList '/i spice-vdagent.msi /qn /norestart' -NoNewWindow -Wait
         Remove-Item spice-vdagent.msi
-        Write-Output "Installing Spice Guest Tools installed successfully!"
+        Write-Output "Spice Guest Tools installed successfully!"
     }
     Catch {
         Write-Output "Unable to install Spice Guest Tools. Error: "
@@ -22,7 +22,7 @@ if ("$env:PACKER_BUILDER_TYPE" -eq "qemu") {
         (New-Object System.Net.WebClient).DownloadFile('https://www.spice-space.org/download/windows/spice-webdavd/spice-webdavd-x64-latest.msi', 'spice-webdav.msi')
         Start-Process -FilePath msiexec -ArgumentList '/i spice-webdav.msi /qn /norestart' -NoNewWindow -Wait
         Remove-Item spice-webdav.msi
-        Write-Output "Installing Spice Guest Tools installed successfully!"
+        Write-Output "Spice WEBDAV Daemon installed successfully!"
     }
     Catch {
         Write-Output "Unable to install Spice WebDAV Daemon. Error: "
